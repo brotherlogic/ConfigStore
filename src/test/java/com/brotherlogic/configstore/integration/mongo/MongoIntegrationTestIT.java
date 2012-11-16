c71d32b2c1c9403916cb21fbfe8b26d8fd6ce1d7
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2012-11-16 23:01:45 +0000

    Fixed unit and integration separation

---
+++ src/test/java/com/brotherlogic/configstore/integration/mongo/MongoIntegrationTestIT.java
@@ -0,0 +1,13 @@
+package com.brotherlogic.configstore.integration.mongo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MongoIntegrationTestIT
+{
+   @Test
+   public void testWebService()
+   {
+      Assert.assertTrue("Reached here", false);
+   }
+}
